4d8e6104a67e160459bf8687ced8a6221401506f
Adiack06 <joshmills2006@outlook.com>
2023-03-13 14:25:54 +0000

    sql.php

---
+++ Website/sql.php
@@ -0,0 +1,11 @@
+<?php
+$host="127.0.0.1";//host ip
+$db_username="localadmin"; //mysql username
+$db_password="Pa$$w0rd01"; //mysql password
+$db_name="store";     //mysql database name e.g AR12345678
+//create connection
+$conn = new mysqli($host, $db_username, $db_password, $db_name);
+if ($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+};
+?>
