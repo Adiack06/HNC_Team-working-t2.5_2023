41c97345eb134db8e1cac92177515a73e37f935a
Adiack06 <joshmills2006@outlook.com>
2023-03-15 13:08:54 +0000

    can now login

--- Website/login.php
+++ Website/login.php
@@ -12,6 +12,7 @@
 	<body>
 		<h1>SQL Results</h1>
 		<?php
+			
 				$username=$_POST['username'];
 				$password=$_POST['password'];
 
@@ -21,27 +22,24 @@
 				//prepare and bind
 				//NOTE we cannot do select * from
 				//we MUST specify what is to be returned!!
-				$stmt = $conn->prepare("SELECT forename,surname,street,town,postcode,email,username,access_level FROM student WHERE username = ? AND password = ?");
+				$stmt = $conn->prepare("SELECT student_id,username,password,email,address FROM student WHERE username = ? AND password = ?");
 
 				$stmt->bind_param("ss", $username, $password);//things to send
 
 				$stmt->execute();
 				$stmt->store_result();
-				$stmt->bind_result($forename,$surname,$street,$town,$postcode,$email,$username,$access_level); //things to retrieve
+				$stmt->bind_result($student_id,$username,$password,$email,$address); //things to retrieve
 
 				$row_count = $stmt->num_rows; //get the number of rows! If it is 1 we are logged in! If 0 we found no match!
 
 				echo 'number of rows.....'.$row_count.'<br>';
 
 				while ($stmt->fetch()) {
-						echo $forename.'<br>';
-						echo $surname.'<br>';
-						echo $street.'<br>';
-						echo $town.'<br>';
-						echo $postcode.'<br>';
-						echo $email.'<br>';
+						echo $student_id.'<br>';
 						echo $username.'<br>';
-						echo $access_level.'<br>';
+						echo $password.'<br>';
+						echo $email.'<br>';
+						echo $address.'<br>';
 				};
 
 				$stmt->close(); //close the sql
@@ -53,24 +51,12 @@
 
 				if ($row_count ==1)
 				  {
-						echo 'Logged in!<br>';
-						$_SESSION['loggedin'] = 'yes';
-						echo '<pre>';
-						print_r($_SESSION);
-						echo '</pre>';
-
-						if($access_level == 1){
-								$_SESSION['admin'] = 'yes';
-								header('location:admin.php');
-						}else{
-								header('location:protected.php');
-						};
-
-
+						header('location:profile.php');
+						
 				  } else {
 						session_unset();
 						session_destroy();
-						header('location:index.html');
+						header('location:index.php');
 				};
 
 				//What about access level?
--- Website/profile.php
+++ Website/profile.php
@@ -1,24 +1,79 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-  header("Location: login.php");
-  exit();
-}
-
-$user_id = $_SESSION['user_id'];
-$username = $_SESSION['username'];
-$email = $_SESSION['email'];
-?>
-
 <!DOCTYPE html>
 <html>
-<head>
-  <title>User Profile</title>
-  <link rel="stylesheet" type="text/css" href="styles/style.css">
-</head>
-<body>
-  <h1>Welcome to your profile, <?php echo $username; ?>!</h1>
-  <p>Your email address is: <?php echo $email; ?></p>
-  <p><a href="logout.php">Logout</a></p>
-</body>
+	<head>
+		<style>
+			#stock {
+			  font-family: Arial, Helvetica, sans-serif;
+			  border-collapse: collapse;
+			  width: 100%;
+			}
+
+			#stock td, #stock th {
+			  border: 1px solid #ddd;
+			  padding: 8px;
+			}
+
+			#stock tr:nth-child(even){background-color: #f2f2f2;}
+
+			#stock tr:hover {background-color: #ddd;}
+
+			#stock th {
+			  padding-top: 12px;
+			  padding-bottom: 12px;
+			  text-align: left;
+			  background-color: #04AA6D;
+			  color: white;
+			}
+		</style>
+	</head>
+	<body>
+	<h1>STOCK</h1>
+	<?php
+		include 'sql.php';//include creds
+		$stmt = $conn->prepare("SELECT stockno,description,price,qtyinstock FROM stock");
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($stockno,$description,$price,$qtyinstock);
+		
+		$row_count = $stmt ->num_rows;
+		
+		echo 'number of rows...'.$row_count.'<br>';
+		
+		echo '<table border = "1" id="stock">
+			<tr>
+				<th>Stock Number</th>
+				<th>Image</th>
+				<th>Description</th>
+				<th>Price</th>
+				<th>Quantity</th>
+				<th>Order</th>
+			</tr>';
+		while ($stmt ->fetch()) {
+			echo '<tr>';
+				echo '<td>'.$stockno.'</td>';
+				echo '<td><img src="images/'.$stockno.'.jpg" alt="'.$description.'" style="width:300px;"></td>';
+				echo '<td>'.$description.'</td>';
+				echo '<td>£'.$price.'</td>';
+				echo '<td>'.$qtyinstock.'</td>';
+				echo '<td>';
+				echo '<select name="'.$stockno.'" form="orderform">';
+				for($i=0;$i<$qtyinstock+1;$i++){
+					echo ' <option value="' . $i . '">' . $i . '</option>';
+				};
+				echo '</select>';
+				echo '</td>';
+				
+			echo '</tr>';
+		};
+		
+		$stmt ->close();
+		$conn ->close();
+		
+		echo '<form action="placeorder.php" method = "post" id="orderform">
+		<input type="submit" value="Order">
+		</form>';
+		
+		echo '</table>';
+	?>
+	</body>
 </html>
