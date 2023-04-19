e9996c33152eed172717f33587b6070337196c30
Jambo4231 <50552472+Jambo4231@users.noreply.github.com>
2023-04-19 21:44:24 +0100

    Create new_product.php
    Modified product.php

---
+++ new_product.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+	<?php 
+		$inventory_id = $_GET['inventory_id'];
+		$title = $_GET['title'];
+		$description = $_GET['description'];
+		$author_brand = $_GET['author_brand'];
+		$form = $_GET['form'];
+		$image_name = $_GET['image_name'];
+		$price = $_GET['price'];
+		$qtyinstock = $_GET['qtyinstock'];
+		$text = $_GET['text'];
+		$stars = $_GET['stars'];
+	?>
+	<h1><?php echo $title; ?></h1>
+	<img src="images/<?php echo $image_name; ?>" alt="<?php echo $description; ?>" style="width:300px;">
+	<p><strong>Description:</strong> <?php echo $description; ?></p>
+	<p><strong>Author/Brand:</strong> <?php echo $author_brand; ?></p>
+	<p><strong>Form:</strong> <?php echo $form; ?></p>
+	<p><strong>Price:</strong> <?php echo $price; ?></p>
+	<p><strong>Quantity in stock:</strong> <?php echo $qtyinstock; ?></p>
+	<?php if ($text && $stars) { ?>
+		<p><strong>Review:</strong> <?php echo $text; ?></p>
+		<p><strong>Stars:</strong> <?php echo $stars; ?></p>
+	<?php } ?>
+	<form action="add_to_basket.php" method="post">
+		<input type="hidden" name="inventory_id" value="<?php echo $inventory_id; ?>">
+		<label for="quantity">Select quantity:</label>
+		<select id="quantity" name="quantity">
+			<?php 
+				for ($i=1; $i<=$qtyinstock; $i++) { 
+					echo "<option value='$i'>$i</option>";
+				}
+			?>
+		</select>
+		<br>
+		<button type="submit">Add to basket</button>
+	</form>
+	<a href="#" onclick="history.back(); return false;">Back</a>
+</body>
+</html>
