8974b8e773f41a5fcc2ad3c671af0fde9dcd6511
jmjsinclair <112957290+jmjsinclair@users.noreply.github.com>
2023-05-03 09:42:35 +0100

    Update products_page.php

--- Website/products_page.php
+++ Website/products_page.php
@@ -33,18 +33,18 @@
 	<h1>inventory</h1>
 	<?php
 		include 'sql.php';//include creds
-		$stmt = $conn->prepare("SELECT inventory_id,price,qtyinstock,image_name,title,author_brand,form FROM inventory");
+		$stmt = $conn->prepare("SELECT inventory_id,description,price,qtyinstock,image_name,title,author_brand,form FROM inventory");
 		$stmt->execute();
 		$stmt->store_result();
-		$stmt->bind_result($inventory_id,$price,$qtyinstock,$image_name,$title,$author_brand,$form);
+		$stmt->bind_result($inventory_id,$description,$price,$qtyinstock,$image_name,$title,$author_brand,$form);
 		
 		$row_count = $stmt ->num_rows;
 		$name_length = 50;
 		echo '<div id="inventory">';
 		while ($stmt->fetch()) {
 			echo '<div>';
-			echo '<img src="images/' . $image_name .'">';
-			echo '<h2><a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . $title . '</a></h2>';
+			echo '<img src="images/' . $image_name . '" alt="' . $description . '">';
+			echo '<h2><a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . $title . '</a></h2>';
 			
 			if (strlen( $title) >= $name_length) {
 				echo '<p>' . substr( $title, 0, $name_length) . '...</p>';
