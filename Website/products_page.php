a9945dd34d9c4af96b4edd8194ec3660dd42a864
jmjsinclair <112957290+jmjsinclair@users.noreply.github.com>
2023-05-03 10:26:07 +0100

    Update products_page.php

--- Website/products_page.php
+++ Website/products_page.php
@@ -32,35 +32,33 @@
 	<body>
 	<h1>inventory</h1>
 	<?php
-	include 'sql.php';//include creds
-	$stmt = $conn->prepare("SELECT inventory_id, description, price, qtyinstock, image_name, title, author_brand, form FROM inventory");
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($inventory_id, $description, $price, $qtyinstock, $image_name, $title, $author_brand, $form);
-	
-	$row_count = $stmt->num_rows;
-	$title_length = 50; // set the desired length of the title
-	echo '<div id="inventory">';
-	while ($stmt->fetch()) {
-    	echo '<div>';
-    	echo '<img src="images/' . $image_name . '" alt="' . $description . '">';
-    	echo '<h2><a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . $title . '</a></h2>';
+		include 'sql.php';//include creds
+		$stmt = $conn->prepare("SELECT inventory_id,description,price,qtyinstock,image_name,title,author_brand,form FROM inventory");
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->bind_result($inventory_id,$description,$price,$qtyinstock,$image_name,$title,$author_brand,$form);
+		
+		$row_count = $stmt ->num_rows;
+		$name_length = 50;
+		echo '<div id="inventory">';
+			while ($stmt->fetch()) {
+    			echo '<div>';
+    			echo '<img src="images/' . $image_name . '" alt="' . $description . '">';
+    			echo '<h2>';
+          if (strlen($title) >= $name_length) {
+      			  echo '<a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . substr($title, 0, $name_length) . '...</a>';
+    			} else {
+      			  echo '<a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . $title . str_repeat('&nbsp;', $name_length - strlen($title)) . '</a>';
+   		 		}
+    			echo '</h2>';
+    			echo '</div>';
+			};
+		echo '</div>';
 
-    	if (strlen($title) < $title_length) {
-    		// add empty space to make titles the same length
-      		echo '<p>' . $title . str_repeat('&nbsp;', $title_length - strlen($title)) . '</p>';
-    	} else {
-      		echo '<p>' . $title . '</p>';
-   	 	}
-
-    	echo '</div>';
-	};
-	echo '</div>';
-
-	$stmt->close();
-	$conn->close();
-	echo '<button onclick="location.href=\'basket.php\'">Basket</button>';
+		
+		$stmt ->close();
+		$conn ->close();
+		echo '<button onclick="location.href=\'basket.php\'">Basket</button>';
 	?>
-
 	</body>
 </html>
