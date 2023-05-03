423ebbeb9e666d951044e3dabfaa484f1093cc9b
jmjsinclair <112957290+jmjsinclair@users.noreply.github.com>
2023-05-03 10:33:40 +0100

    Update products_page.php

--- Website/products_page.php
+++ Website/products_page.php
@@ -48,7 +48,7 @@
           if (strlen($title) >= $name_length) {
       			  echo '<a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . substr($title, 0, $name_length) . '...</a>';
     			} else {
-      			  echo '<a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . $title . str_repeat('&nbsp;', $name_length - strlen($title)) . '</a>';
+      			  echo '<h2><a href="product.php?inventory_id=' . $inventory_id . '&title=' . $title . '&description=' . $description . '&author_brand=' . $author_brand . '&form=' . $form . '&image_name=' . $image_name . '&price=' . $price . '&qtyinstock=' . $qtyinstock . '">' . $title;
    		 		}
     			echo '</h2>';
     			echo '</div>';
